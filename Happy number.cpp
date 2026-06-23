66053d5de1bf7731fc290beffc7eb632eea88764
Himanshu Sharma <himanshuitm27@gmail.com>
2026-06-23 15:52:46 +0530

    Create Happy number.cpp
    Leetcode - 202

---
+++ Happy number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+int nextNumber(int n ){
+    int sum = 0 ;
+
+    while(n>0){
+        int digit = n %10 ;
+
+        sum += digit * digit ;
+        n/=10 ;
+    }
+    return sum ;
+}
+
+    bool isHappy(int n) {
+        int slow = n ;
+        int fast = nextNumber(n);
+
+        while(fast != 1 && slow != fast ){
+            slow = nextNumber(slow);
+            fast = nextNumber(nextNumber(fast));
+        }
+
+        return fast == 1 ;
+
+
+    }
+        
+};
