1bfb6c2f4445ea49b566c447c44a61b95348f2d8
Himanshu Sharma <himanshuitm27@gmail.com>
2026-06-24 12:52:38 +0530

    Reverse  Vowels of a string.cpp
    345. Reverse Vowels of a String
    Solved
    Easy
    
    Topics
    premium lock icon
    Companies
    Given a string s, reverse only all the vowels in the string and return it.
    
    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
    
     
    
    Example 1:
    
    Input: s = "IceCreAm"
    
    Output: "AceCreIm"
    
    Explanation:
    
    The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
    
    Example 2:
    
    Input: s = "leetcode"
    
    Output: "leotcede"
    
     
    
    Constraints:
    
    1 <= s.length <= 3 * 105
    s consist of printable ASCII characters.

---
+++ Reverse  Vowels of a string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int n = s.length();
+        int i = 0 ;
+        int j = n-1 ;
+
+        while(i<j){
+            if(!isVowel(s[i])) i++;
+            else if(!isVowel(s[j])) j--;
+            else{
+                swap(s[i] , s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s ;
+
+    }
+
+
+    bool isVowel(char & ch){
+        return ch=='a' || ch == 'e' ||
+        ch == 'i' || ch == 'o'||
+        ch == 'u' ||
+        ch == 'A' || ch == 'E' ||
+        ch == 'I' || ch == 'O' ||
+        ch == 'U'; 
+    }
+};
