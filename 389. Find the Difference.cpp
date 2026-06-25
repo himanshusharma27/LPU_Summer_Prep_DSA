c80df5b59bc2fc5411ba1bed0e4109c17fec1e27
Himanshu Sharma <himanshuitm27@gmail.com>
2026-06-25 10:28:20 +0530

    Create  389. Find the Difference.cpp
     Find the Difference
    Solved
    Easy
    
    Topics
    premium lock icon
    Companies
    You are given two strings s and t.
    
    String t is generated by random shuffling string s and then add one more letter at a random position.
    
    Return the letter that was added to t.
    
     
    
    Example 1:
    
    Input: s = "abcd", t = "abcde"
    Output: "e"
    Explanation: 'e' is the letter that was added.
    Example 2:
    
    Input: s = "", t = "y"
    Output: "y"
     
    
    Constraints:
    
    0 <= s.length <= 1000
    t.length == s.length + 1
    s and t consist of lowercase English letters.

---
+++ 389. Find the Difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum_s = 0 ;
+
+        for(char ch : s){ 
+            sum_s += ch ;
+        }
+
+        int sum_t = 0 ;
+
+        for(char ch : t){
+            sum_t += ch;
+        }
+
+        return sum_t - sum_s;
+    }
+};
