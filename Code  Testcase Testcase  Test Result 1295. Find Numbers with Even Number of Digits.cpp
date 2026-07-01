be6ab8f89db5992eb57c2f18ad4eb511d7751b03
Himanshu Sharma <himanshuitm27@gmail.com>
2026-07-01 11:13:11 +0530

    1295. Find Numbers with Even Number of Digits.cpp
    Hint
    Given an array nums of integers, return how many of them contain an even number of digits.
    
     
    
    Example 1:
    
    Input: nums = [12,345,2,6,7896]
    Output: 2
    Explanation: 
    12 contains 2 digits (even number of digits). 
    345 contains 3 digits (odd number of digits). 
    2 contains 1 digit (odd number of digits). 
    6 contains 1 digit (odd number of digits). 
    7896 contains 4 digits (even number of digits). 
    Therefore only 12 and 7896 contain an even number of digits.
    Example 2:
    
    Input: nums = [555,901,482,1771]
    Output: 1 
    Explanation: 
    Only 1771 contains an even number of digits.
     
    
    Constraints:
    
    1 <= nums.length <= 500
    1 <= nums[i] <= 105

---
+++ Code  Testcase Testcase  Test Result 1295. Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0 ;
+
+        for (int n : nums){
+            string s = to_string(n);
+
+            if(s.length() % 2 == 0){
+                count++;
+            }
+        }
+        return count ;
+    }
+};
